3cb84c2396c84a595212c058d10df5fae4351990
mathe-u <matheussilvades@gmail.com>
2025-11-03 18:30:51 -0300

    add more fields to user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,14 +2,21 @@ class User {
   final int id;
   final String username;
   final String email;
+  final bool isActive;
 
-  User({required this.id, required this.username, required this.email});
+  User({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.isActive,
+  });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      id: json['user_id'] ?? 0,
+      id: json['id'] ?? 0,
       username: json['username'] ?? '',
       email: json['email'] ?? '',
+      isActive: json['is_active'] ?? false,
     );
   }
 }
